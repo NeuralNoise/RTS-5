c0d9d58846018d2d5c0f26f92e78a898f4fe514c
Sam Marion <sam.marion@franwell.com>
2017-02-24 16:58:28 -0500

    added UI for selection of units and beginning of unit selection logif

---
+++ Assets/UnitSelector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitSelector : MonoBehaviour {
+
+    public GameObject CommandCenterMenu;
+    private bool commandMenuVisable = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            commandMenuVisable = !commandMenuVisable;
+            CommandCenterMenu.SetActive(commandMenuVisable);
+        }
+	}
+}
